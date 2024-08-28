4cd5a1d65446e41379d1a66448fbad8335fbc565
Nguyen Duc Thinh <thinhdz1500@gmail.com>
2024-08-28 11:16:29 +0700

    update ex3

--- Week1/ex3.js
+++ Week1/ex3.js
@@ -16,8 +16,50 @@ else
 // team only wins if it has a higher score than the other team, and the same time a 
 // score of at least 100 points. Hint: Use a logical operator to test for minimum 
 // score, as well as multiple else-if blocks 
-const dolpins = [97, 112, 101]
-const koalas = [88, 91, 110]
+console.log("Cau 3");
+
+let dolpins = [97, 112, 101]
+let koalas = [88, 91, 110]
+let checkMinimun100 = (animal)=>{
+    for (let i = 0; i < animal.length; i++) {
+        if (animal[i] < 100) {
+            return false;
+        }
+    }
+    return true;
+} 
+let average = (animal)=>{
+    let sum = 0;
+    for(let i = 0; i < animal.length; i++){
+        sum += animal[i];
+    }
+}
+if(checkMinimun100(dolpins)||checkMinimun100(koalas)){
+    if(average(dolpins) > average(koalas)){
+        console.log("dolpins is winner");
+    }else if(average(dolpins) < average(koalas)){
+        console.log("koalas is winner");
+    }
+}
+else{
+    console.log("no one win");
+    
+}
     // 4.  Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
     // both teams have the same score and both have a score greater or equal 100 
     // points. Otherwise, no team wins the trophy
+    console.log("Cau 4");
+koalas = [109, 95 ,106];
+if(checkMinimun100(dolpins) || checkMinimun100(koalas)){
+    if(average(dolpins) > average(koalas)){
+        console.log("dolpins is winner");
+    }else if(average(dolpins) < average(koalas)){s
+        console.log("koalas is winner");
+    }else if(average(koalas) == average(dolpins)){
+        console.log("draw");
+    }
+}
+else{
+    console.log("no one win");
+    
+}
