5b4762ca995528e9325302b38c28c3b08792dce2
thinhdz1500 <thinhdz1500@gmail.com>
2024-10-20 16:00:57 +0700

    Week7 update function search notes

--- Week7/app/(tabs)/noteApp.tsx
+++ Week7/app/(tabs)/noteApp.tsx
@@ -39,7 +39,7 @@ const NoteApp = ({ navigation, route }: { navigation: any, route: any }) => {
                     <AntDesign name="checkcircleo" size={20} color="#4AA56A" />
                     <Text style={{ flex: 1, marginLeft: 20 }}>{item.title}</Text>
                     <Pressable onPress={() => {
-                        navigation.navigate('updateNote', { item: item, email: email,refresh:refresh })
+                        navigation.navigate('updateNote', { item: item, email: email, refresh: refresh })
                     }}>
                         <EvilIcons name="pencil" size={24} color="black" />
                     </Pressable>
@@ -59,21 +59,34 @@ const NoteApp = ({ navigation, route }: { navigation: any, route: any }) => {
                 console.error("error fetching data: ", error);
             })
     }, [refresh])
+    const [searchQuery, setSearchQuery] = useState<string>('')
     return (
         <View style={{ flex: 1, margin: 10 }}>
             <View style={{ flexDirection: 'row', borderWidth: 1, width: '100%', height: 44, borderRadius: 10, padding: 10, marginBottom: 30 }}>
                 <EvilIcons name="search" size={24} color="black" />
-                <TextInput placeholder="Search" style={{ marginLeft: 10, width: '100%', }} />
+                <TextInput placeholder="Search" style={{ marginLeft: 10, width: '100%', }}
+                    value={searchQuery}
+                    onChangeText={text => setSearchQuery(text)}
+                />
             </View>
+
             <FlatList
                 data={notes}
-                renderItem={({ item }) => item.email === email ? <Item item={item} /> : (
-                    null)}
+                renderItem={({ item }) => {
+                    if (item.email === email) {
+                        if (searchQuery === '') {
+                            return <Item item={item} />;
+                        } else if (item.title.toLowerCase().includes(searchQuery.toLowerCase())) {
+                            return <Item item={item} />;
+                        }
+                    }
+                    return null; // Trả về null nếu không thỏa mãn điều kiện
+                }}
                 keyExtractor={(item) => item.id.toString()}
             />
             <View style={{ justifyContent: 'center', alignItems: 'center' }}>
                 <Pressable style={{ width: 49, height: 49, borderRadius: 20, borderWidth: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#26C3D9' }}
-                    onPress={() => { navigation.navigate('updateNote', { isNewNote: true, email: email,refresh:refresh }) }}
+                    onPress={() => { navigation.navigate('updateNote', { isNewNote: true, email: email, refresh: refresh }) }}
                 >
                     <Text style={{ fontSize: 22, color: 'white' }}>+</Text>
                 </Pressable>
