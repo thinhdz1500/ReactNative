16b8ef78317c4d81ce420804d67c802b05027325
thinhdz1500 <thinhdz1500@gmail.com>
2024-11-01 08:31:27 +0700

    demo redux tokit api

---
+++ week10/app/presentation/Toolkit/slice.tsx
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+  return response.data;
+});
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState: {
+    todos: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodos.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.todos = action.payload;
+      })
+      .addCase(fetchTodos.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default todosSlice.reducer;
